00e3451f9a1d7735de082510f1fb58ee60826fd2
Drabinskyi Vasiliy <facial91@gmail.com>
2025-10-17 21:38:22 +0300

    Added Program.cs

--- Project/Program.cs
+++ Project/Program.cs
@@ -2,5 +2,19 @@ namespace DefaultNamespace;
 
 public class Program
 {
-    
+    Console.Write("Enter any number (numbers only): ");
+    string userInput = Console.ReadLine();
+
+    bool inputValidation = int.TryParse(userInput, out int number);
+
+    try
+    {
+        if(inputValidation == false)
+            throw new Exception("You entered an incorrect value.");
+        Console.WriteLine($"Result: {number}");
+    }
+    catch (Exception exception)
+    {
+        Console.WriteLine(exception.Message);
+    }
 }
